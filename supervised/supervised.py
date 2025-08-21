eb7eeabbd8809a1b717c2692d6fa765a95de9122
AlessandroPiroddi1 <Alessandro.Piroddi1@it.ey.com>
2025-08-21 15:04:46 +0200

    feat: creazione di matrice di correlazione e analisi visiva con esclusione variabili categoriche(dummy variables)

---
+++ supervised/supervised.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+
+
+df = pd.read_csv('dataset\heart_preprocessed.csv')
+
+
+
+# # Calcola la matrice di correlazione
+# correlation_matrix = df.corr()
+
+# # Visualizza la matrice
+# print(correlation_matrix)
+
+# sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
+# plt.show()
+
+
+# # #### Selezione delle feature
+# X = df[["hour", "dayofweek", "month"]]
+# y = df["target"]
+
+# # #### Split del dataset
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X, y, stratify=y, test_size=0.25, random_state=42
+# )
